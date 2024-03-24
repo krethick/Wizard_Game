d6160f47dccfc84382058d5804ef4bbc264e7387
krethick <krethick@gmail.com>
2024-03-24 16:01:33 +0530

    Finished the basics

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 // Weapon : MonoBehaviour means Weapon inherits from MonoBehaviour
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] // This show the rotationSpeed in the Inspector of the Game object(Weapon) but can't be modified.
+    private int rotationSpeed = 200;
     // Start is called before the first frame update
+
+    [SerializeField]
+    private Vector3 rotationPoint = Vector3.zero;
     void Start()
     {
-        Update();
+        
     }
 
     // Update is called once per frame
@@ -23,6 +28,8 @@ public class Weapon : MonoBehaviour
         // Right click ans select go to definition to check out the variable required
 
         // Using .(dot) allows us to access data and behaviors of any object defined in our scripts
-        transform.RotateAround(Vector3.zero, Vector3.forward, 200 * Time.deltaTime);
+         
+        float rotationAmount = rotationSpeed * Time.deltaTime;
+        transform.RotateAround(rotationPoint, Vector3.forward, rotationAmount);
     }
 }
